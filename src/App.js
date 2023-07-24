3cd80d78cf49c7560995c98948b9597b8d4295be
“Emeka <ngwe7236@gmail.com>
2023-07-24 08:06:23 +0200

    not done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import contacts from "./contacts.json";
+import { FaTimes } from "react-icons/fa";
 import "./App.css";
+
+
+
 
 function App() {
   const [contactList, setContactList] = useState(contacts.slice(0, 5));
   console.log(contactList);
+ 
 
-const [counter, setCounter] = useState (contacts.slice(5));
-const increaseCounter = () => {
-  setCounter(counter + 1) ; 
+const deleteContact = (id) => {
+   setContactList(contactList.filter((contactid) => contactid.id !==id));
+
 }
 
+// const toggleReminder = (toggleid) => {
+// setContactList(
+//   contactList.map((contact) => 
+//   contact.toggleid === toggleid ? {...contact, reminder : !contact.reminder} : contact
+//   )
+// )
+// }
+
+
+
+const [addContact, setaddContacts] = useState (contacts.slice(5));
+
+const addContacttoList = () => {
+  setaddContacts(
+    addContact[parseInt(Math.random() * addContact.length + 1)]
+  )
+
+}
+
+
+
+const deleteNewContact = (id) => {
+  setaddContacts(contactList.filter((newcontactid) => newcontactid.id !==id));
+
+}
 
 
 
   return (
     <div className="App">
 
-      <button onClick={increaseCounter}>Add Random Contact</button>
+      <button onClick={addContacttoList}>Add Random Contact</button>
    
       <h1>IronContact</h1>
 
@@ -43,25 +73,29 @@ const increaseCounter = () => {
                 <td>{contact.popularity}</td>
                 <td>{contact.wonOscar? <img alt="" src="https://github.githubassets.com/images/icons/emoji/unicode/1f3c6.png" /> :""} </td>
                 <td>{contact.wonEmmy}</td>
+              
+                <FaTimes style={{color: 'red', cursor: 'pointer'} } 
+               onClick= {() => deleteContact(contact.id)}/>
               </tr>
             );
           })}
         </tbody>
 
         <tbody>
-        {counter.map((counterList) => {
-      return (
-              <tr key={counterList._id}>
+    
+              <tr key={addContact.id}>
                 <td>
-                  <img alt="" src={counterList.pictureUrl} height = {100} />
+                  <img alt="" key={uuidv4} src={addContact.pictureUrl} height = {100} />
                 </td>
-                <td>{counterList.name}</td>
-                <td>{counterList.popularity}</td>
-                <td>{counterList.wonOscar? <img alt="" src="https://github.githubassets.com/images/icons/emoji/unicode/1f3c6.png" /> :""} </td>
-                <td>{counterList.wonEmmy}</td>
+                <td>{addContact.name}</td>
+                <td>{addContact.popularity}</td>
+                <td>{addContact.wonOscar? <img alt="" src="https://github.githubassets.com/images/icons/emoji/unicode/1f3c6.png" /> :""} </td>
+                <td>{addContact.wonEmmy}</td>
+
+                <FaTimes style={{color: 'red', cursor: 'pointer'} } 
+               onClick= {() => deleteNewContact(addContact.id)}/>
               </tr>
-      );
-            })}
+    
 
         </tbody>
       </table>
